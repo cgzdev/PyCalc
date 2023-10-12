63f6ccc9a8a482e51ae48678597c60a519391cd8
incognia <incognia@gmail.com>
2023-10-11 20:01:48 -0600

    Añadir resta

--- pycalc.py
+++ pycalc.py
@@ -2,8 +2,14 @@
 numero1 = float(input("Ingrese el primer número: "))
 numero2 = float(input("Ingrese el segundo número: "))
 
-# Realizar la suma
-suma = numero1 + numero2
+# Solicitar al usuario que elija una operación (suma o resta)
+operacion = input("Elija una operación (suma o resta): ")
 
-# Mostrar el resultado
-print(f"La suma de {numero1} y {numero2} es: {suma}")
+if operacion.lower() == "suma":
+    resultado = numero1 + numero2
+    print(f"La suma de {numero1} y {numero2} es: {resultado}")
+elif operacion.lower() == "resta":
+    resultado = numero1 - numero2
+    print(f"La resta de {numero1} y {numero2} es: {resultado}")
+else:
+    print("Operación no válida. Por favor, elija 'suma' o 'resta'.")
